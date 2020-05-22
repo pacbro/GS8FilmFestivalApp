19f871a0a8c18c7fc7c83a0ad0acd6a07eb2c994
Joseph on Laptop <j.vincenzi@bellevuecollege.edu>
2020-05-21 17:40:00 -0700

    fixed: default video & added mobile friendly view

--- gs8app/src/components/Carousel.js
+++ gs8app/src/components/Carousel.js
@@ -45,7 +45,27 @@ class Carousel extends Component {
     }
 
     componentDidMount() {
+        /*
+        componentDidMount() is invoked immediately after a component is mounted (inserted
+        into the tree). Initialization that requires DOM nodes should go here. If you need
+        to load data from a remote endpoint, this is a good place to instantiate the network
+        request.
+
+        This method is a good place to set up any subscriptions. If you do that, don’t forget
+         to unsubscribe in componentWillUnmount().
+
+        You may call setState() immediately in componentDidMount(). It will trigger an extra
+        rendering, but it will happen before the browser updates the screen. This guarantees
+        that even though the render() will be called twice in this case, the user won’t see
+        the intermediate state. Use this pattern with caution because it often causes
+        performance issues. In most cases, you should be able to assign the initial state
+        in the constructor() instead. It can, however, be necessary for cases like modals
+        and tooltips when you need to measure a DOM node before rendering something that
+        depends on its size or position.
+        */
+        setFilmSRC('jvq7-b-Ko1g');
         console.log('<< Carousel 01: Mounted successfully >>');
+
     }
 
     /*
@@ -61,8 +81,8 @@ class Carousel extends Component {
                 <iframe
                     id="vid_frame"
                     style={styles.vidContainerIframe}
-                    title="Figure Man Written by Anthony Thambynayagam"
-                    src="http://www.youtube.com/embed/jvq7-b-Ko1g?autoplay=0&rel=0&showinfo=0"
+                    title="Georgetown Super 8 Film Archive"
+                    src="https://www.youtube.com/embed/jvq7-b-Ko1g?autoplay=1&rel=0&showinfo=0&autohide=1"
                     frameBorder="0"
                     width="640"
                     height="480"
