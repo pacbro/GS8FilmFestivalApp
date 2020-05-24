b034e86f0ffc1bf529fe41239c36dbd55b400210
Joseph on Laptop <j.vincenzi@bellevuecollege.edu>
2020-05-23 17:49:52 -0700

    Server set to use app.listen()

--- server/index.js
+++ server/index.js
@@ -15,25 +15,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Remember to remove 'cors' from package.json
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/FilmArchiveSrv/static', express.static('public'));
 
 console.log('## Launching GS8 server ##');
 db.on('error', console.error.bind(console, '** GS8 DB connection error:'));
 
 app.get('/', (req, res) => {
-    res.send('Hello Worldz!');
+    res.send('Hello Worldz! - GS8/');
 });
-app.get('/contact', (req, res) => {
-    res.send('Hello World!');
+app.get('/FilmArchiveSrv', (req, res) => {
+    res.send('Hello Worldz! - GS8/FilmArchiveSrv');
 });
-app.get('/subscribe', (req, res) => {
-    res.send('Hello World!');
-});
-
+//app.get('/contact', (req, res) => {res.send('Hello World!');});
+//app.get('/subscribe', (req, res) => {res.send('Hello World!');});
 // to be handled by other sources
 //app.get('/filmpurchase', (req, res) => {res.send('Hello World!')});
 //app.get('/donate', (req, res) => {res.send('Hello World!')})
+//app.use('/tags', tagsRouter);
 
-app.use('/tags', tagsRouter);
 //app.listen(apiPort, () => console.log('## Server running on port ${apiPort} ##'));
 app.listen(() => console.log('## GS8 Server is now running ##'));
 
