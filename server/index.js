d574c9a65e33b76cc20b9510dc7d94d1affde93b
Joseph on Laptop <j.vincenzi@bellevuecollege.edu>
2020-05-23 18:45:12 -0700

    Server log fixed to show port.

--- server/index.js
+++ server/index.js
@@ -31,28 +31,3 @@ app.get('/FilmArchiveSrv', (req, res) => {
 
 app.listen(apiPort, () => console.log(`## GS8/FilmArchiveSrv running on port ${apiPort} ##`));
 
-
-
-
-
-/*
-//////////////////////////////////////////////////////////////////
-// This version of server code is for testing basic functionality.
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('views engin', 'ejs');
-app.use('/FilmArchiveSrv/static', express.static('public'));
-
-console.log('## Launching GS8 server ##');
-
-app.get('/FilmArchiveSrv', (req, res) => {
-    res.send('Hello Worldz! - FAS');
-});
-
-app.listen();
-// this version of server code is for testing basic funvtionality.
-//////////////////////////////////////////////////////////////////
-*/
