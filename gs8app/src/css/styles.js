81f7a6f635e2e83106b150161e17040d643aef6a
Joseph Vincenzi <j.vincenzi@bellevuecollege.edu>
2020-05-26 05:50:34 -0700

    Update styles.js
    CSS Update border: 3px => 1px,

--- gs8app/src/css/styles.js
+++ gs8app/src/css/styles.js
@@ -33,7 +33,7 @@ const nextBackDiv = {
 const logos = {
     width: '86px',
     height: '110px',
-    border: '3px solid #ccc',
+    border: '1px solid #ccc',
     padding: '5px',
     //margin: '-9px',
     margin: '-3px -9px 0px -9px',
@@ -45,7 +45,7 @@ const logos = {
 const logo2011 = {
     width: '160px',
     height: '110px',
-    border: '3px solid #ccc',
+    border: '1px solid #ccc',
     padding: '5px',
     //margin: '-9px',
     margin: '-3px -9px 0px -9px',
