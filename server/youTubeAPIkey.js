451c4277a90b05b8612ac4d4dbbbf17deb6ea318
Joseph Vincenzi <j.vincenzi@bellevuecollege.edu>
2020-05-26 02:39:34 -0700

    Delete youTubeAPIkey.js
    oops

--- server/youTubeAPIkey.js
+++
@@ -1,8 +0,0 @@
-const YOUTUBE_API_KEY = 'AIzaSyC1I7W2jhhxH2ek_NjR3QHS7AD2Zh43m5Q';
-module.exports = YOUTUBE_API_KEY;
-/*
-https://www.googleapis.com/youtube/v3/videos?key=AIzaSyAs32sLueoowi0liOQBxz9bO26porA4jLk&fields=items(snippet(channelId,title,description,tags,publishedAt))&dets=contentDetails&part=snippet&id=5s03TfKYgS8
- */
-//https://developers.google.com/youtube/v3/docs/videos
-
-
